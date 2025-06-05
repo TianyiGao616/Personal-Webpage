e0e10e7bea5626585f3dcc899a1402893825a518
Tianyi Gao <144143604+TianyiGao616@users.noreply.github.com>
2025-06-05 14:34:10 -0400

    costume footer with my info.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -10,21 +10,13 @@ interface Options {
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-  © {year} All rights reserved by Tianyi Gao · 
-  <a href="https://github.com/TianyiGao616" target="_blank" rel="noopener noreferrer">GitHub</a> · 
-  <a href="https://www.linkedin.com/in/tianyi-gao-631126260" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-</p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        © {year} All rights reserved by Tianyi Gao · 
+        <a href="https://github.com/TianyiGao616" target="_blank" rel="noopener noreferrer">GitHub</a> · 
+        <a href="https://www.linkedin.com/in/tianyi-gao-631126260" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </p>
       </footer>
     )
   }
