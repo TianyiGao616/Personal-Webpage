3896afbb5ed75e2ac0eee5265da629b250453f4f
Tianyi Gao <144143604+TianyiGao616@users.noreply.github.com>
2025-06-05 13:06:30 -0400

    update the page title.

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
-    pageTitleSuffix: "Tianyi Gao",
+    pageTitle: "Tianyi Gao",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
